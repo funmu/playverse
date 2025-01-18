60dccdaa7c58e75f3e5d3fdb572e44f489cd2cfc
funmu <muralirk@gmail.com>
2025-01-18 15:36:11 -0800

    add sources and additional methods for news component provider

--- facenews/news_component/component.py
+++ facenews/news_component/component.py
@@ -45,8 +45,22 @@ class CustomComponent(Component):
     @operation()
     def hello(self) -> str: pass 
 
+    # get all top headlines
     @operation()
-    def headlines( self) -> str: pass
+    def headlines( self, country="us") -> str: pass
 
+    # get the latest news for given area of scope
     @operation()
-    def latest( self, scope: str) -> list[NewsItem]: pass
+    def latest( self, scope: str="technology") -> str: pass
+
+    # get the headlines by country
+    @operation()
+    def headlinesByCountry( self, country="us"): pass
+
+    # get the sources for getting news from
+    @operation()    
+    def sources( self): pass
+    
+    # get the headlines by source; default is from reuters
+    @operation()    
+    def headlinesBySource( self, source="reuters"): pass
