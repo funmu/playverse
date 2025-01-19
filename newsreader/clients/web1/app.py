275e1908a69ac5ee37cf396bbe42263b67e76441
funmu <muralirk@gmail.com>
2025-01-18 18:26:24 -0800

    extend app.py to load news from various sources

--- newsreader/clients/web1/app.py
+++ newsreader/clients/web1/app.py
@@ -7,31 +7,91 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # MY SERVER ... get this from environment file
-MY_NEWS_SERVER='https://myServer/'  # use for testing purposes
+MY_NEWS_SERVER='http://localhost:8080/'  # use for testing purposes
+server_location = os.getenv('MY_NEWS_SERVER') 
 
 NewsNotFoundHTML = "newsNotFound.html"
-NewsTemplateHTML = 'index.html'
+HeadlineNewsTemplateHTML = 'index.headlines.html'
+NewsHomeTemplateHTML = 'index.html'
+
+NEWS_SOURCES = {
+    "reuters"       : "Reuters",
+    "associated-press": "Asspciated Press",
+    "ap"            : "Associated Press",
+    "bloomberg"     : "Bloomberg News",
+    "fortune"       : "Fortune",
+    "newsweek"      : "Newsweek",
+    "new-york-magazine" : "New York Magazine",
+    "new-scientist" : "New Scientist",
+
+    "abc-news"      : "ABC News",
+    "bbc-news"      : "BBC News",
+    "cbs-news"      : "CBS News",
+    "nbc-news"      : "National Broadcasting Corporation (NBC)",
+    "cnn"           : "CNN (Cable News Network)",
+    "cnbc"          : "CNBC",
+    "msnbc"         : "Microsoft + National Broadcasting Corporation (NBC)",
+
+    "buzzfeed"      : "Buzzfeed",
+    "engadget"      : "Engadget",
+    "ars-technica"  : "Ars Technica",
+    "google-news"   : "Google News",
+    "hacker-news"   : "Hacker News",
+    "techcrunch"    : "TechCrunch",
+    "gizmodo"       : "Gizmodo",
+}
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template( NewsHomeTemplateHTML, sources=NEWS_SOURCES)
+
+@app.route('/hello')
+def index_hello():
+    print( "\n---------- \nGOT a test request")
+    return "Hello World!"
+
+@app.route('/headlines')
+def index_headlines():
+    return show_headlines()
+
+@app.route('/headlines/source')
+def index_headlines_by_source():
+    """
+    This route handles requests to /headlines with optional query parameters.
+
+    Example usage:
+    /headlines/source?source=<SOURCENAME>&scope=<SCOPE>
+    """
+    source = request.args.get('source', 'reuters')
+    scope = request.args.get('scope', '')
+
+    headline_args = { "source": source}
+    if (scope != ''):
+        headline_args['scope']=scope
+
+    return show_headlines( 'headlinesBySource', headline_args)
+
+def show_headlines( operation = "headlines", headline_args = {}):
     print( "\n---------- \nGetting the news articles")
-    newsItems = getNewsItems()
+    newsItems = getNewsItems( operation, op_args=headline_args)
     if ( newsItems != None):
         articles = newsItems["result"]["articles"]
         articles = filterOutPartialItems( articles)
         print( f"\t{len(articles)} articles are found")
-        return render_template( NewsTemplateHTML, items=articles)
+        if ( len(articles) > 0):
+            return render_template( HeadlineNewsTemplateHTML, items=articles)
+        else:
+            return render_template( NewsNotFoundHTML)
     else:
         print( "GOT ERROR")
         return render_template( NewsNotFoundHTML)
 
-def getNewsItems():
+def getNewsItems( operation, op_args = {}):
     try:
         # Get the news data using a POST request
         # Get the server location from the .env file
-        server_location = os.getenv('MY_NEWS_SERVER') 
         if not server_location:
             print( f"Error: SERVER_LOCATION not found in .env file.")
             return None
@@ -43,10 +103,12 @@ def getNewsItems():
         }
         data = {
             "operation": {
-                "name": "headlines",
-                "args": {}
+                "name": operation,
+                "args": op_args
             }
         }
+
+        print( f"Sending request to {server_location}\n\theaders: {headers}\n\tdata: {data}");
         response = requests.post(url, headers=headers, json=data)
         response.raise_for_status()  # Raise an exception for bad status codes
 
@@ -57,13 +119,18 @@ def getNewsItems():
         print(f"Error fetching news data: {e}")
         return None
 
+def filterOutPartialItems( articles: [] ) -> []:
+    filtered_items = [item for item in articles if item.get('title') != "[Removed]"] 
+    return filtered_items
 
-# Sample data (replace with your actual data)
-sample_items = [
-    {'property1': 'value1', 'property2': 'value2', 'property3': 'value3'},
-    {'property1': 'value4', 'property2': 'value5', 'property3': 'value6'},
-    {'property1': 'value7', 'property2': 'value8', 'property3': 'value9'},
-]
+## ---- ---- ---- ---- ---- ---- ---- ---- ----
+# MAIN CODE is here
+
+if __name__ == '__main__':
+    app.run(debug=True)    
+
+## ---- ---- ---- ---- ---- ---- ---- ---- ----
+# Following are for testing purposes only 
 
 @app.route('/sample')
 def index_sample():
@@ -71,14 +138,16 @@ def index_sample():
 
 @app.route('/file1')
 def index_file1():
-    return render_from_file( 'data/2024-12-15.headlines.json', 'index.html')
+    source_for_news = 'data/2024-12-15.headlines.json'
+    # source_for_news = 'data/sample1.json' # for testing purposes
+    return render_from_file( source_for_news, HeadlineNewsTemplateHTML)
 
-    # for testing:
-    #     return render_from_file( 'data/sample1.json', 'index.html')
-
-def filterOutPartialItems( articles: [] ) -> []:
-    filtered_items = [item for item in articles if item.get('title') != "[Removed]"] 
-    return filtered_items
+# Sample data (replace with your actual data)
+sample_items = [
+    {'property1': 'value1', 'property2': 'value2', 'property3': 'value3'},
+    {'property1': 'value4', 'property2': 'value5', 'property3': 'value6'},
+    {'property1': 'value7', 'property2': 'value8', 'property3': 'value9'},
+]
 
 def render_from_file( jsonDataFile: str, templateHTML: str):
     try:
@@ -93,6 +162,3 @@ def render_from_file( jsonDataFile: str, templateHTML: str):
         return f"Error: JSON {jsonDataFile} file not found."
     except json.JSONDecodeError:
         return f"Error: Invalid JSON format {jsonDataFile} in file."
-
-if __name__ == '__main__':
-    app.run(debug=True)    
